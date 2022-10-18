9a4f0222fb842bb68e72a021ef27166d6f4f117c
samuel ip <samuel.c.ip@student.uts.edu.au>
2022-10-18 18:48:19 +1100

    0%

---
+++ Assets/Scripts/GravityScene/GravityManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityManager : MonoBehaviour {
+    public enum GravityDirection { Up, Down, Left, Right }
+    public static Vector3[] GravityVectors;
+
+
+    void Awake () {
+        GravityVectors = new Vector3[]{ Vector3.up, Vector3.down, Vector3.left, Vector3.right };
+	}
+}
