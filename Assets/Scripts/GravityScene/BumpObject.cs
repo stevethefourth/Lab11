eda14ae0144e6c410797aa043c183893d1f1f19f
amageton23 <111858381+stevethefourth@users.noreply.github.com>
2022-10-20 19:23:53 +1100

    80% complete

--- Assets/Scripts/GravityScene/BumpObject.cs
+++ Assets/Scripts/GravityScene/BumpObject.cs
@@ -5,19 +5,69 @@ using UnityEngine;
 public class BumpObject : MonoBehaviour
 {
     private Rigidbody rigid;
+    public GravityManager.GravityDirection gravDirection;
+    public float bumpForce = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
         rigid = gameObject.GetComponent<Rigidbody>();
         if (!gameObject.GetComponent<Rigidbody>())
         {
-            gameObject.AddComponent<Rigidbody>();
+            rigid = gameObject.AddComponent<Rigidbody>();
         }
-    }
+        if (gravDirection != GravityManager.GravityDirection.Up)
+        {
+            rigid.useGravity = false;
+        }
 
-    // Update is called once per frame
+    }
     void Update()
     {
+       
+        switch (gravDirection)
+        {
+            case GravityManager.GravityDirection.Down:                
+                if (Input.GetKeyDown(KeyCode.Space))
+                    rigid.AddForce(Vector3.up * bumpForce, ForceMode.Impulse);               
+                break;
+            case GravityManager.GravityDirection.Right:                
+                if (Input.GetKeyDown(KeyCode.Space))
+                    rigid.AddForce(Vector3.left * bumpForce, ForceMode.Impulse);                
+                break;
+            case GravityManager.GravityDirection.Left:                
+                if (Input.GetKeyDown(KeyCode.Space))
+                    rigid.AddForce(Vector3.right * bumpForce, ForceMode.Impulse);                
+                break;
+            case GravityManager.GravityDirection.Up:
+                if (Input.GetKeyDown(KeyCode.Space))
+                    rigid.AddForce(Vector3.down * bumpForce, ForceMode.Impulse);              
+                break;
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        switch (gravDirection)
+        {
+            case GravityManager.GravityDirection.Down:
+                rigid.AddForce(Vector3.down * GravityManager.GravityStrength);
+                break;
+            case GravityManager.GravityDirection.Right:
+                rigid.AddForce(Vector3.right * GravityManager.GravityStrength);               
+                break;
+            case GravityManager.GravityDirection.Left:
+                rigid.AddForce(Vector3.left * GravityManager.GravityStrength);              
+                break;
+            case GravityManager.GravityDirection.Up:                
+                break;
+        }
+
+        
         
     }
+            
 }
+
+    
+
--- Assets/Scripts/GravityScene/GravityManager.cs
+++ Assets/Scripts/GravityScene/GravityManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GravityManager : MonoBehaviour {
     public enum GravityDirection { Up, Down, Left, Right }
     public static Vector3[] GravityVectors;
-    public static float GravityStrength = 9.80665f;
+    public static float GravityStrength = 10f;
 
     void Awake () {
         GravityVectors = new Vector3[]{ Vector3.up*GravityStrength, Vector3.down, Vector3.left, Vector3.right };
